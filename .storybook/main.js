33020031b5717756cd84bc2e67bc756e586fee00
aoyagi <aoyagisan@hotmail.com>
2023-04-08 11:59:17 +0900

    chore: unified description

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 const toPath = (filePath) => path.join(process.cwd(), filePath);
 
 module.exports = {
-  "webpackFinal": async (config) => {
+  webpackFinal: async (config) => {
     return {
       ...config,
       resolve: {
@@ -16,14 +16,14 @@ module.exports = {
       },
     }
   },
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
       "@storybook/addon-links",
       "@storybook/addon-essentials",
       "@storybook/addon-interactions",
       "@storybook/addon-mdx-gfm"
   ],
-  "framework": {
+  framework: {
     name: "@storybook/nextjs",
     options: {}
   },
